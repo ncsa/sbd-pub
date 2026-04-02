1e74ad341d480b764e7c1b70b9b78ff7073c44da
Bob Walkup <walkup@us.ibm.com>
2026-04-02 08:24:30 -0400

    added GPU offload for reduced density matrix computation

--- include/sbd/chemistry/basic/correlation.h
+++ include/sbd/chemistry/basic/correlation.h
@@ -4,6 +4,11 @@
 */
 #ifndef SBD_CHEMISTRY_BASIC_CORRELATION_H
 #define SBD_CHEMISTRY_BASIC_CORRELATION_H
+
+#ifdef USE_OMP_OFFLOAD
+#include "../basic/omp_offload.h"
+#endif
+
 namespace sbd {
   /**
      Function for adding diagonal contribution
@@ -38,6 +43,51 @@ namespace sbd {
       }
     }
   }
+
+#pragma omp declare target
+  template <typename ElemT>
+  void ZeroDiffCorrelation(size_t * DetI,
+                           ElemT WeightI,
+                           size_t bit_length,
+                           size_t norb,
+                           ElemT * oneBody,
+                           ElemT * twoBody) {
+    int closed[SBD_MAX_SPINORBITALS];
+    int num_closed = 0;
+    for (int i = 0; i < 2*norb; i++) {
+       int block = i / bit_length;
+       int pos   = i % bit_length;
+       if ((DetI[block] >> pos) & 1) {
+          closed[num_closed] = i;
+          num_closed++;
+       }
+    }
+
+    size_t norb2 = norb * norb;
+    size_t norb4 = norb2 * norb2;
+
+    for(int i=0; i < num_closed; i++) {
+      int oi = closed[i]/2;
+      int si = closed[i]%2;
+#pragma omp atomic update
+      oneBody[si*norb2 + oi + norb*oi] += Conjugate(WeightI)*WeightI;
+      for(int j=i+1; j < num_closed; j++) {
+        int oj = closed[j]/2;
+        int sj = closed[j]%2;
+#pragma omp atomic update
+        twoBody[(si+2*sj)*norb4 + oi+norb*oj+norb*norb*oi+norb*norb*norb*oj] += Conjugate(WeightI) * WeightI;
+#pragma omp atomic update
+        twoBody[(sj+2*si)*norb4 + oj+norb*oi+norb*norb*oj+norb*norb*norb*oi] += Conjugate(WeightI) * WeightI;
+        if( si == sj ) {
+#pragma omp atomic update
+          twoBody[(si+2*sj)*norb4 + oi+norb*oj+norb*norb*oj+norb*norb*norb*oi] += -Conjugate(WeightI) * WeightI;
+#pragma omp atomic update
+          twoBody[(sj+2*si)*norb4 + oj+norb*oi+norb*norb*oi+norb*norb*norb*oj] += -Conjugate(WeightI) * WeightI;
+        }
+      }
+    }
+  }
+#pragma omp end declare target
 
   /**
      Function for adding one-occupation different contribution
@@ -80,6 +130,55 @@ namespace sbd {
       }
     }
   }
+
+#pragma omp declare target
+  template <typename ElemT>
+  void OneDiffCorrelation(const size_t * DetI,
+                          const ElemT WeightI,
+                          const ElemT WeightJ,
+                          const size_t bit_length,
+                          const size_t norb,
+                          int i,
+                          int a,
+                          ElemT * oneBody,
+                          ElemT * twoBody) {
+    int detSize = (2*norb + bit_length - 1) / bit_length;
+    double sgn = 1.0;
+    parity_device(DetI,bit_length,std::min(i,a),std::max(i,a),sgn);
+    size_t norb2 = norb * norb;
+    size_t norb4 = norb2 * norb2;
+    int oi = i / 2;
+    int si = i % 2;
+    int oa = a / 2;
+    int sa = a % 2;
+#pragma omp atomic update
+    oneBody[si*norb2 + oi+norb*oa] += Conjugate(WeightI) * WeightJ * ElemT(sgn);
+    size_t one = 1;
+    for(int x=0; x < detSize; x++) {
+      size_t bits = DetI[x];
+      while(bits != 0) {
+        int pos = __builtin_ffsl(bits);
+        int soj = x * bit_length + pos - 1;
+        int oj = soj / 2;
+        int sj = soj % 2;
+
+#pragma omp atomic update
+        twoBody[(si+2*sj)*norb4 + oa+oj*norb+oi*norb*norb+oj*norb*norb*norb] += Conjugate(WeightI) * WeightJ * ElemT(sgn);
+#pragma omp atomic update
+        twoBody[(sj+2*si)*norb4 + oj+oa*norb+oj*norb*norb+oi*norb*norb*norb] += Conjugate(WeightI) * WeightJ * ElemT(sgn);
+
+        if( si == sj ) {
+#pragma omp atomic update
+          twoBody[(si+2*sj)*norb4 + oa+oj*norb+oj*norb*norb+oi*norb*norb*norb] += Conjugate(WeightI) * WeightJ * ElemT(-sgn);
+#pragma omp atomic update
+          twoBody[(sj+2*si)*norb4 + oj+oa*norb+oi*norb*norb+oj*norb*norb*norb] += Conjugate(WeightI) * WeightJ * ElemT(-sgn);
+        }
+
+        bits &= ~(one << (pos-1));
+      }
+    }
+  }
+#pragma omp end declare target
 
   /**
      Function for adding two-occupation different contribution
@@ -124,6 +223,55 @@ namespace sbd {
     }
 
   }
+
+#pragma omp declare target
+  template <typename ElemT>
+  void TwoDiffCorrelation(const size_t * DetI,
+                          const ElemT WeightI,
+                          const ElemT WeightJ,
+                          const size_t bit_length,
+                          const size_t norb,
+                          int i,
+                          int j,
+                          int a,
+                          int b,
+                          ElemT * oneBody,
+                          ElemT * twoBody) {
+    size_t norb2 = norb * norb;
+    size_t norb4 = norb2 * norb2;
+    double sgn = 1.0;
+    int I = std::min(i,j);
+    int J = std::max(i,j);
+    int A = std::min(a,b);
+    int B = std::max(a,b);
+    parity_device(DetI,bit_length,std::min(I,A),std::max(I,A),sgn);
+    parity_device(DetI,bit_length,std::min(J,B),std::max(J,B),sgn);
+    if( A > J || B < I ) sgn *= -1.0;
+    int oi = I / 2;
+    int si = I % 2;
+    int oa = A / 2;
+    int sa = A % 2;
+    int oj = J / 2;
+    int sj = J % 2;
+    int ob = B / 2;
+    int sb = B % 2;
+
+    if( si == sa ) {
+#pragma omp atomic update
+      twoBody[(si+2*sj)*norb4 + oa+norb*ob+norb*norb*(oi+norb*oj)] += ElemT(sgn) * Conjugate(WeightI) * WeightJ;
+#pragma omp atomic update
+      twoBody[(sj+2*si)*norb4 + ob+norb*oa+norb*norb*(oj+norb*oi)] += ElemT(sgn) * Conjugate(WeightI) * WeightJ;
+    }
+
+    if( si == sb ) {
+#pragma omp atomic update
+      twoBody[(si+2*sj)*norb4 + oa+norb*ob+norb*norb*(oj+norb*oi)] += ElemT(-sgn) * Conjugate(WeightI) * WeightJ;
+#pragma omp atomic update
+      twoBody[(sj+2*si)*norb4 + ob+norb*oa+norb*norb*(oi+norb*oj)] += ElemT(-sgn) * Conjugate(WeightI) * WeightJ;
+    }
+
+  }
+#pragma omp end declare target
 
   /**
      Function for adding the terms to the resulting correlation
